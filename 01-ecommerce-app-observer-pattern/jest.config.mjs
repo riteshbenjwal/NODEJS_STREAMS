700e398fc1844e62f4702b8380f5a046e6de8911
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-05-20 14:54:58 +0530

    observer pattern intro

---
+++ 01-ecommerce-app-observer-pattern/jest.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: ["text", "lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
